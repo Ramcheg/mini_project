5075cc1668da5440d9d95e4099b24e8ee8e2d7a4
Ramcheg <qwerasvlad@gmail.com>
2019-07-09 15:52:50 +0300

    adaptiv 60%

--- js/script.js
+++ js/script.js
@@ -60,14 +60,10 @@ $(document).ready(function(){
 
 
 
-	// external js: isotope.pkgd.js
-
-// init Isotope
 var $grid = $('.grid').isotope({
   itemSelector: '.element-item',
   layoutMode: 'fitRows'
 });
-// filter functions
 var filterFns = {
 
   ium: function() {
@@ -76,14 +72,11 @@ var filterFns = {
   },
 };
 
-// bind filter button click
 $('.filters-button-group').on( 'click', 'button', function() {
   var filterValue = $( this ).attr('data-filter');
-  // use filterFn if matches value
   filterValue = filterFns[ filterValue ] || filterValue;
   $grid.isotope({ filter: filterValue });
 });
-// change is-checked class on buttons
 $('.button-group').each( function( i, buttonGroup ) {
   var $buttonGroup = $( buttonGroup );
   $buttonGroup.on( 'click', 'button', function() {
